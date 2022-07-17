200590b9c32511fd786b8987759e7ff9a06ae53c
Nicholas Barkote <barkotenicholas@gmail.com>
2022-07-17 19:51:54 +0300

    Added code to navigate to emoji fragment

--- app/src/main/java/emo/barkote/emoji/MainActivity.kt
+++ app/src/main/java/emo/barkote/emoji/MainActivity.kt
@@ -2,10 +2,22 @@ package emo.barkote.emoji
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import emo.barkote.emoji.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFrag = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        val navController = navHostFrag.navController
+
+
+
+
     }
 }
--- app/src/main/java/emo/barkote/emoji/SplashScreen.kt
+++ app/src/main/java/emo/barkote/emoji/SplashScreen.kt
@@ -1,10 +1,14 @@
 package emo.barkote.emoji
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.navOptions
 import emo.barkote.emoji.databinding.FragmentSplashScreeenBinding
 
 
@@ -12,16 +16,28 @@ class SplashScreen : Fragment() {
 
     private lateinit var binding: FragmentSplashScreeenBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
-       binding = FragmentSplashScreeenBinding.inflate(inflater,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSplashScreeenBinding.inflate(inflater, container, false)
 
 
-
-
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            val navHostFragment =
+                activity?.supportFragmentManager?.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController.navigate(R.id.action_splashScreen_to_emoji, null,
+                navOptions { // Use the Kotlin DSL for building NavOptions
+                    anim {
+                        enter = android.R.animator.fade_in
+                        exit = android.R.animator.fade_out
+                    }
+                })
+        }, 3000)
         return binding.root
     }
-
 
 
 }
