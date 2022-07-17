e1442ba2db56840fe8ad2dd3b143d5893925739f
Nicholas Barkote <barkotenicholas@gmail.com>
2022-07-17 09:59:28 +0300

    first commit

---
+++ app/src/main/java/emo/barkote/emoji/MainActivity.kt
@@ -0,0 +1,11 @@
+package emo.barkote.emoji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
